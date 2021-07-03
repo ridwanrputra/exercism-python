d9a770459891df715376937657550b412abcf7d7
ridwan <ridwan.rputra@gmail.com>
2021-07-04 00:40:40 +0700

    luhn functional

--- luhn/luhn.py
+++ luhn/luhn.py
@@ -1,21 +1,22 @@
-import itertools
-
-
 class Luhn:
     def __init__(self, card_num):
-        self.card_num = card_num.replace(" ", "")[::-1]
+        self.card_num = card_num.replace(" ", "")
 
     def valid(self):
-        if len(self.card_num) <= 1 or not self.card_num.isnumeric():
-            return False
+        def luhny_tune(num):
+            return num * 2 - 9 if num * 2 >= 10 else num * 2
 
-        return (
-            sum(
-                map(
-                    lambda x: x if x < 10 else x - 9,
-                    [   int(elm) * 2 if idx % 2 != 0 else int(elm)
-                        for idx, elm in enumerate(self.card_num)
-                    ],
-                )
-            ) % 10 == 0
-        )
+        def luhny_bin(pos, sum, chars):
+            if not chars:
+                return False if pos <= 1 else sum % 10 == 0
+            head, *tail = chars
+            if not head.isdigit():
+                return False
+            if pos % 2 == 0:
+                return luhny_bin(pos + 1, sum + int(head), tail)
+            else:
+                return luhny_bin(pos + 1, sum + luhny_tune(int(head)), tail)
+
+        return luhny_bin(0, 0, list(self.card_num[::-1]))
+
+
