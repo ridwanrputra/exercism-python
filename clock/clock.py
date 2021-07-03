c679dfa6b90e3494fd2a43b03cc0c1ede49bbe3c
ridwan <ridwan.rputra@gmail.com>
2021-07-03 23:46:08 +0700

    luhn aproved

---
+++ clock/clock.py
@@ -0,0 +1,16 @@
+class Clock:
+    def __init__(self, hour, minute):
+        self.hour = (hour + minute // 60) % 24
+        self.minute = minute % 60
+
+    def __repr__(self):
+        return "{hh:02d}:{mm:02d}".format(hh=self.hour, mm=self.minute)
+
+    def __eq__(self, other):
+        return str(self) == str(other)
+
+    def __add__(self, minutes):
+        return Clock(self.hour, self.minute + minutes)
+
+    def __sub__(self, minutes):
+        return Clock(self.hour, self.minute - minutes)
